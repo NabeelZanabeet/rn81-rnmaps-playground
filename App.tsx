196977831fdbdd163a5b749fe45f23c7ed08f1f6
nabeel <nabeel@get-a-way.com>
2025-09-25 01:11:07 +0300

    Reproducing Marker issue not rendering rn Image component

--- App.tsx
+++ App.tsx
@@ -5,12 +5,16 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+  View,
+  Image,
+} from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import gasStation from './assets/images/gas.png';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -24,21 +28,52 @@ function App() {
 }
 
 function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
   return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+    <View style={styles.root}>
+      <View style={styles.container}>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        >
+          <Marker
+            stopPropagation
+            tracksViewChanges={false}
+            coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
+            // image={{uri: 'map_pin_gasstation'}}
+          >
+            {/*If u comment out below child Image, then default red marker is displayed */}
+            <Image
+              style={{
+                width: 16,
+                height: 16,
+              }}
+              source={gasStation}
+              resizeMode="contain"
+            />
+          </Marker>
+        </MapView>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
+  },
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
 
